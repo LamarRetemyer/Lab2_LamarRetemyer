da6caf7fe06b411cd27da851e1f6051b1c12ad26
Lamar Retemyer <lamar.retemyer@gmail.com>
2024-02-19 18:46:00 -0400

    Added the AC class and tested the mutators

---
+++ AirConditioner/AirConditioner/Main.java
@@ -0,0 +1,15 @@
+package AirConditioner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        AirConditioner ac1 = new AirConditioner();
+         ac1.setUnits();
+        System.out.println(ac1.getUnits());
+        ac1.setUnits();
+        System.out.println(ac1.getUnits());
+       
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab2_LamarRetemyer
+This is Lab 2 for Lamar Retemyer in COMP2232.
